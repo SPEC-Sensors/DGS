abf9797ab0f57d03dc0fb61933d0abdc69fa902d
David Peaslee <dpeaslee@spec-sensors.com>
2021-03-29 18:07:19 -0500

    Update DSDK_SETUP.ino
    added function examples

--- examples/DSDK_SETUP/DSDK_SETUP.ino
+++ examples/DSDK_SETUP/DSDK_SETUP.ino
@@ -1,8 +1,8 @@
 /*
   DSDK_SETUP - Example use of library for reading KWJ Engineering with SPEC Sensors on Digital SDK.
   Created by David E. Peaslee, March 29, 2018.
+  Updated by David Peaslee, March 29, 2021.
   Created for SPEC/KWJ Digital Gas Sensor (DGS, or also known as DULPSM)
-  Not yet released into the public domain.
 
 
   !!! RX, TX Must be on 3.3 volt communication, or using level shifters to get to 3.3V UART!!!
@@ -12,6 +12,8 @@
   Connect the RX of Digital Sensor to the TX of 3.3 V Arduino
   Connect the power of Digital Sensor to the 3.3V power of the Arduino
   Connect the ground of Digital Sensor to the ground of the Arduino
+
+  Serial terminal settings: 9600 baud, 8N1N, "no line endings"
 
   This code is optimized for the Arduino Due, but can be modified to work with any other Arduino.
   The DGS is a 3.3V device, which means you must use level shifters if using with a 5V Arduino.
@@ -34,7 +36,8 @@
 //SoftwareSerial mySerial(10, 11); // !!! RX, TX Must be on 3.3 volt communication, or using level shifters to get to 3.3V UART!!!
 
 DGS mySensor(&Serial1);
-
+const char* Barcode = "042617040460 110102 CO 1705 2.72";
+char input;
 void setup() //Setup intended for first time setup of SDK. Must initialize both serial ports:
 {
   Serial.begin(9600);
@@ -43,60 +46,78 @@ void setup() //Setup intended for first time setup of SDK. Must initialize both 
   Serial1.flush();
   Serial.println("Begin Setup");
 
-  mySensor.DEBUG = false;
+  mySensor.DEBUG = true;
+
 
   //Remainder of setup() code is only necessary for initial setup of DULP
+
   Serial.println();
   Serial.println(mySensor.getFW());  // Retruns FW date
-  Serial.println();
+  Serial.println(mySensor.setBC(Barcode));
 
-  if (mySensor.setBC("071615011247 110102 CO 1507 4.05")) Serial.println("Finished Setting BC");  //Enter your barcode string. Returns 1/0.
+  Serial.println(mySensor.setToff(1.03)); delay(500);
 
-  mySensor.getEEPROM();  //Read eeprom into array and/or list view if DEBUG is TRUE. See end of sketch for outline of values.
-  Serial.println();
+  Serial.println(mySensor.getLMP());  // Retruns LMP Settings
+  Serial.println(mySensor.setLMP(mySensor.LMP[0], mySensor.LMP[1], mySensor.LMP[2]));
+  delay(1000);
+  mySensor.getEEPROM();
 
-  if (mySensor.getLMP()) { // Loads LMP values into variable returns 1/0
-    Serial.println("LMP91000 Values loaded into array");
-    if (mySensor.setLMP(mySensor.LMP[0], mySensor.LMP[1], mySensor.LMP[2])) Serial.println("Finished Setting New Values For LMP"); //Sets LMP variables into register returns 1/0
-  }
-  Serial.println();
-
-  if (mySensor.setToff(2.3)) Serial.println("Finished Setting T Offset"); //Set T offset in deg C returns 1/0
-  Serial.println();
 
   Serial.println("Enter 'Z' When Sensor is Stabilized");
   while (Serial.read() != 'Z') {}
   if (mySensor.zero()) Serial.println("Finished Setting Zero"); //zeros returns 1/0
-  Serial.println();
 
   Serial.println("Finished Setup");
+  Serial.println("1/0 (read): raw counts, concentration (ppb), integer temp (degC), float temp (degF), float temp (degC), int humid (%), float humid (%)");
+
 }
 
 void loop()
 {
-  mySensor.getData('\r');
-  Serial.print("Concentration = ");
-  Serial.print(mySensor.getConc());     //default is 'p' for temperature compensated ppb, any other cahracter for raw counts
-  Serial.print(", Temperature = ");
-  Serial.print(mySensor.getTemp('F'));  //'F' or 'C' for units of temperature, default is 'C', any other cahracter for raw counts
-  Serial.print(", Humidity = ");
-  Serial.println(mySensor.getRh());     //default is 'r' for %Rh, any other character for raw counts
-
-  while (Serial.available()) // read from Serial port, send to port SDK port
-  {
-    char U = Serial.read();
-    if (U == 'Z') {
-      if (mySensor.zero()) Serial.println("Finished Setting Zero"); //zeros returns 1/0
-    }
-    if (U == 'f') {
-      Serial.println(mySensor.getFW());
-    }
-    if (U == 'e') {
-      mySensor.DEBUG = true;
-      mySensor.getEEPROM();
+  if (Serial.available()) {
+    input = Serial.read();
+    switch (input) {
+      case 'e':
+        mySensor.getEEPROM(); //print eeprom
+        break;
+      case 's':
+        //sleep for 30 seconds, need call to wake to see if time elapsed
+        mySensor.sleepSensor(30);
+        //for open-ended sleep, mySensor.sleepSensor(); then 2 calls to mySensor.getData('\r') to wake
+        break;
+      case 'z':
+        mySensor.zero();  //rezero sensor
+        break;
+      case 'a':
+        mySensor.setAve(100);
+        break;
+      default:
+        break;
     }
   }
+
+  if (mySensor.wakeSensor()) {// check if awake, wake if sleep time elapsed
+
+
+    delay(1000);
+    Serial.print(mySensor.getData('\r'));
+    Serial.print(": ");
+    Serial.print(mySensor.getConc('c'));
+    Serial.print(", ");
+    Serial.print(mySensor.getConc());
+    Serial.print(", ");
+    Serial.print((int) mySensor.getTemp()); //same as getTemp('c')
+    Serial.print(", ");
+    Serial.print(mySensor.getTemp('F'));
+    Serial.print(", ");
+    Serial.print(mySensor.getTemp('C'));
+    Serial.print(", ");
+    Serial.print((int) mySensor.getRh()); //same as getRh('r')
+    Serial.print(", ");
+    Serial.println(mySensor.getRh('R'));
+  }
 }
+
 
 /*
     EEPROM Values can be called using the following addresses (Ex. mySensor.e[1] is int and mySensor.E[1] is String)
