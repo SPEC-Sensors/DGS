e2cc302cadb2089de865f2cdfbfbc530c8549014
David Peaslee <dpeaslee@spec-sensors.com>
2021-03-29 18:05:29 -0500

    Update DGS.h
    added some functions

--- src/DGS.h
+++ src/DGS.h
@@ -15,6 +15,58 @@ Updated by David Peaslee, MAR 24, 2021
 //#include "WProgram.h"
 #endif
 
+class DGS
+{
+  private:
+    Stream *_mySerial;
+    long dataArray[11]; //Multipurpose array
+    void DEBUG_PRINTLN(long x);
+    void DEBUG_PRINTLN(float x);
+    void DEBUG_PRINTLN(String x);
+    void DEBUG_PRINT(long x);
+    void DEBUG_PRINT(float x);
+    void DEBUG_PRINT(String x);
+    void unlock(void);
+	
+	unsigned long sleepTime;
+
+
+  public:
+    DGS(Stream &mySerial); //Class that represents DGS with Stream as class for Serial
+    DGS(Stream *mySerial); //Class that represents DGS with Stream as class for Serial
+
+    bool DEBUG;
+
+	bool isSleeping;
+	void sleepSensor(unsigned long sleepy = 0);
+	bool wakeSensor(void);
+	
+	int setAve(int t);
+	
+	int setToff(float offset);  //Sets a temperature offset
+    int setBC(const char *BC);  //Initializes DGS with all information from barcode, only way to set Span
+    String getFW(void); //Returns the Firmware Date
+	
+	
+    int getLMP(void); //Loads LMP[3] with register values from LMP91000
+    int LMP[3];	//LMP Registers, needs to be loaded with getLMP
+    int setLMP(int R1, int R2, int R3); //Sets new LMP registers and reintializes LMP
+
+    int zero(void); //Zeros to current reading, Zero only in a clean air environment
+    int setXSpan(float); //ReCalibrates device, only should be used after zero, and using a calibrated gas
+
+    int getData(char c); //Gets the data and loads into variables
+    long getConc(char u = 'p');  //Reads concentration from current data array 'p' ppb or 'c' counts
+    float getTemp(char u = 'c');  //Reads temperature from current data array 'C' for Celsius or 'F' for Fahrenheit
+    float getRh(char u = 'r'); //Reads Rh from current data array
+
+    void getEEPROM(void); //Loads variables into EEPROM E[5] and e[14] arrays, outputs EEPROM if DEBUG is true
+    String eepromStr[5]; //Stores the character based EEPROM data
+    long eepromInt[13];  //Stores the integer based EEPROM data
+    float Sensitivity_Code;  //Stores the sensitivity coefficient in nA/PPM
+};
+
+
 /*
 EEPROM Values can be called using the following addresses (Ex. CO.e[1] is int and CO.E[1] is String)
 nA_per_PPM_x100 = e[1]
@@ -37,46 +89,5 @@ Gas = E[3]
 Date_Code = E[4]
 Sensitivity_Code = Sf
 */
-
-class DGS
-{
-  private:
-    Stream *_mySerial;
-    long dataArray[11]; //Multipurpose array
-    void unlock(void);
-    void DEBUG_PRINTLN(long x);
-    void DEBUG_PRINTLN(float x);
-    void DEBUG_PRINTLN(String x);
-    void DEBUG_PRINT(long x);
-    void DEBUG_PRINT(float x);
-    void DEBUG_PRINT(String x);
-
-  public:
-    DGS(Stream &mySerial); //Class that represents DGS with Stream as class for Serial
-    DGS(Stream *mySerial); //Class that represents DGS with Stream as class for Serial
-
-    bool DEBUG;
-
-    int setToff(float offset);  //Sets a temperature offset
-    int setBC(char *BC);  //Initializes DGS with all information from barcode, only way to set Span
-    String getFW(void); //Returns the Firmware Date
-
-    int getLMP(void); //Loads LMP[3] with register values from LMP91000
-    int LMP[3];	//LMP Registers, needs to be loaded with getLMP
-    int setLMP(int R1, int R2, int R3); //Sets new LMP registers and reintializes LMP
-
-    int zero(void); //Zeros to current reading, Zero only in a clean air environment
-    int setXSpan(float); //ReCalibrates device, only should be used after zero, and using a calibrated gas
-
-    int getData(char c); //Gets the data and loads into variables
-    long getConc(char c = 'p');  //Reads concentration from current data array 'p' ppb or 'c' counts
-    long getTemp(char u = 'C');  //Reads temperature from current data array 'C' for Celsius or 'F' for Fahrenheit
-    long getRh(void); //Reads Rh from current data array
-
-    void getEEPROM(void); //Loads variables into EEPROM E[5] and e[14] arrays, outputs EEPROM if DEBUG is true
-    String eepromStr[5]; //Stores the character based EEPROM data
-    long eepromInt[13];  //Stores the integer based EEPROM data
-    float Sensitivity_Code;  //Stores the sensitivity coefficient in nA/PPM
-};
 
 #endif
